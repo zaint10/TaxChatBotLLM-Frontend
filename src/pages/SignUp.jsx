8572427cd6d1dfd73f15aa747faf59b9545a9674
Zain <zat938@gmail.com>
2024-04-30 15:23:05 +0500

    if applied, this commit rm env files from repo

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -3,13 +3,14 @@ import axios from "axios";
 import SignUp from "../components/SignUp";
 import { useNavigate } from "react-router-dom";
 import useSignIn from "react-auth-kit/hooks/useSignIn";
+import apiService from "../service/apiService";
 
 const SignUpPage = () => {
   const navigate = useNavigate();
   const signIn = useSignIn();
   const handleSubmit = async (email, password) => {
     try {
-      const response = await axios.post("http://localhost:8000/signup/", {
+      const response = await apiService.post("/signup/", {
         email,
         password,
       });
